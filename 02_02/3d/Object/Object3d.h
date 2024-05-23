05b09af928e997e43f1e00dce86cca31ef76cebb
Souto-Naitou <146906423+Souto-Naitou@users.noreply.github.com>
2024-05-23 23:31:13 +0900

    # [02_02] - デバッグウィンドウをタブ化

---
+++ 02_02/3d/Object/Object3d.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include <stdint.h>
+#include <Matrix4x4.h>
+#include <Vector3.h>
+
+struct Sphere
+{
+	Vector3 center;
+	float radius;
+};
+
+struct Line
+{
+	Vector3 origin;	// 始点
+	Vector3 diff;	// 終点への差分ベクトル
+};
+
+struct Ray
+{
+	Vector3 origin;	// 始点
+	Vector3 diff;	// 終点への差分ベクトル
+};
+
+struct Segment
+{
+	Vector3 origin;	// 始点
+	Vector3 diff;	// 終点への差分ベクトル
+};
+
+struct Plane
+{
+	Vector3 normal; // 法線
+	float distance; // 距離
+};
+
+Vector3 Perpendicular(const Vector3& _vector);
+
+
+void DrawSphere(const Sphere& _sphere, const Matrix4x4& _viewProjectionMatrix, const Matrix4x4& _viewportMatrix, uint32_t _color);
+void DrawPlane(const Plane& _plane, const Matrix4x4& _viewProjectionMatrix, const Matrix4x4& _viewportMatrix, uint32_t _color);
+
+bool IsCollision(const Sphere& _s1, const Sphere& _s2);
+bool IsCollision(const Sphere& _sphere, const Plane& _plane);
---
+++ 02_02/main.cpp
@@ -0,0 +1,195 @@
+#include <Novice.h>
+#include "Vector3/calc/vector3calc.h"
+#include "Vector3/output/vector3out.h"
+#include "Matrix4x4/calc/matrix4calc.h"
+#include "Matrix4x4/output/matrix4out.h"
+#include <stdint.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <ImGuiManager.h>
+#include "3d/DebugFunction/Debug3D.h"
+#include "3d/Object/Object3d.h"
+
+const char kWindowTitle[] = "学籍番号";
+const int kWindowWidth = 1280;
+const int kWindowHeight = 720;
+
+const float cameraSpeed = 0.01f;
+const Vector3 defaultRot = {0.0f, 0.0f, 0.0f};
+
+void	Initialize();
+void	ImGuiWnd();
+void	Update();
+void	Draw();
+
+char keys[256] = { 0 };
+char preKeys[256] = { 0 };
+
+Matrix4x4	cameraMatrix{};
+Matrix4x4	viewMatrix{};
+Matrix4x4	projectionMatrix{};
+Matrix4x4	viewportMatrix{};
+Matrix4x4	viewProjectionMatrix{};
+Vector3		cameraPosition{};
+Vector3		cameraRotation{};
+
+Sphere		sphere1{};
+Vector3		s1Scale{};
+Matrix4x4	s1WorldMatrix{};
+
+Plane		plane{};
+
+bool		isCollision = false;
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
+
+	Initialize();
+
+	while (Novice::ProcessMessage() == 0) {
+		Novice::BeginFrame();
+
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		ImGuiWnd();
+		Update();
+		Draw();
+
+		Novice::EndFrame();
+
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	Novice::Finalize();
+	return 0;
+}
+
+void Initialize()
+{
+	// 初期化
+	cameraPosition.x = 0.0f;
+	cameraPosition.y = 1.9f;
+	cameraPosition.z = -6.49f;
+
+	cameraRotation.x = 0.26f;
+	cameraRotation.y = 0.0f;
+	cameraRotation.z = 0.0f;
+
+	sphere1.radius = 1.0f;
+	plane.normal.y = 1.0f;
+	plane.normal = Normalize(plane.normal);
+	plane.distance = 1.0f;
+}
+
+void ImGuiWnd()
+{
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0);
+	ImGui::PushStyleColor(ImGuiCol_WindowBg, ImU32(0xff080808));
+	ImGui::PushStyleColor(ImGuiCol_TitleBgActive, ImU32(0xff0C27D8));
+
+	/// Window
+	ImGui::SetNextWindowSize(ImVec2(300, 320));
+	const ImGuiWindowFlags windowFlag = ImGuiWindowFlags_NoResize;
+	ImGui::Begin("DebugWindow", (bool*)false, windowFlag);
+
+	if (ImGui::BeginTabBar("ObjectsTab"))
+	{
+		if (ImGui::BeginTabItem("Camera"))
+		{
+			ImGui::Spacing();
+			ImGui::Text("Translate");
+			ImGui::SliderFloat("X", &cameraPosition.x, -20.0f, 20.0f);
+			ImGui::SliderFloat("Y", &cameraPosition.y, -20.0f, 20.0f);
+			ImGui::SliderFloat("Z", &cameraPosition.z, -20.0f, 20.0f);
+
+			ImGui::Spacing();
+			ImGui::Separator();
+			ImGui::Spacing();
+			
+			ImGui::Text("Rotation");
+			ImGui::SliderFloat("X", &cameraRotation.x, -2 * float(M_PI), 2 * float(M_PI));
+			ImGui::SliderFloat("Y", &cameraRotation.y, -2 * float(M_PI), 2 * float(M_PI));
+			ImGui::SliderFloat("Z", &cameraRotation.z, -2 * float(M_PI), 2 * float(M_PI));
+
+			ImGui::EndTabItem();
+		}
+
+		if (ImGui::BeginTabItem("Sphere")) {
+
+			ImGui::Spacing();
+
+			ImGui::Text("Transform");
+
+			ImGui::SliderFloat("X", &sphere1.center.x, -3.0f, 3.0f);
+			ImGui::SliderFloat("Y", &sphere1.center.y, -3.0f, 3.0f);
+			ImGui::SliderFloat("Z", &sphere1.center.z, -3.0f, 3.0f);
+
+			ImGui::SliderFloat("Radius", &sphere1.radius, 0.001f, 3.0f);
+
+			ImGui::EndTabItem();
+		}
+
+		if (ImGui::BeginTabItem("Plane")) {
+
+			ImGui::Spacing();
+
+			ImGui::Text("Transform");
+
+			if (ImGui::DragFloat3("Normal", &plane.normal.x, 0.01f))
+			{
+				plane.normal = Normalize(plane.normal);
+			}
+			ImGui::DragFloat("Distance", &plane.distance, 0.01f);
+			
+
+			ImGui::EndTabItem();
+		}
+
+		ImGui::EndTabBar();
+	}
+
+	ImGui::End();
+
+	ImGui::PopStyleColor();
+	ImGui::PopStyleColor();
+	ImGui::PopStyleVar();
+	///
+}
+
+void Update()
+{
+	if (keys[DIK_SPACE]) cameraPosition.y += cameraSpeed;
+	if (keys[DIK_LSHIFT]) cameraPosition.y -= cameraSpeed;
+	if (keys[DIK_A]) cameraPosition.x -= cameraSpeed;
+	if (keys[DIK_D]) cameraPosition.x += cameraSpeed;
+	if (keys[DIK_S]) cameraPosition.z -= cameraSpeed;
+	if (keys[DIK_W]) cameraPosition.z += cameraSpeed;
+
+	if (keys[DIK_UPARROW]) cameraRotation.x -= cameraSpeed;
+	if (keys[DIK_DOWNARROW]) cameraRotation.x += cameraSpeed;
+	if (keys[DIK_LEFTARROW]) cameraRotation.y -= cameraSpeed;
+	if (keys[DIK_RIGHTARROW]) cameraRotation.y += cameraSpeed;
+	if (keys[DIK_SEMICOLON]) cameraRotation.z -= cameraSpeed;
+	if (keys[DIK_COLON]) cameraRotation.z += cameraSpeed;
+
+	if (cameraPosition.z == 0) cameraPosition.z = 0.001f;
+	cameraMatrix = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, cameraRotation, cameraPosition);
+	viewMatrix = Inverse(cameraMatrix);
+	projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
+	viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
+	viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
+
+	isCollision = IsCollision(sphere1, plane);
+}
+
+void Draw()
+{
+	DrawGrid(viewProjectionMatrix, viewportMatrix);
+	DrawSphere(sphere1, viewProjectionMatrix, viewportMatrix, isCollision ? RED : WHITE);
+	DrawPlane(plane, viewProjectionMatrix, viewportMatrix, WHITE);
+}
