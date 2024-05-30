d106b0dd2646d833d7d9d175324f9df4365c1182
Souto-Naitou <146906423+Souto-Naitou@users.noreply.github.com>
2024-05-30 23:56:38 +0900

    02_04.ver1

---
+++ 02_04/3d/Object/Object3d.h
@@ -0,0 +1,65 @@
+﻿#pragma once
+#include <stdint.h>
+#include <Matrix4x4.h>
+#include <Vector3.h>
+
+struct Sphere
+{
+	Vector3 center;
+	float radius;
+};
+
+/// <summary>
+/// 直線
+/// </summary>
+struct Line
+{
+	Vector3 origin;	// 始点
+	Vector3 diff;	// 終点への差分ベクトル
+};
+
+/// <summary>
+/// 半直線
+/// </summary>
+struct Ray
+{
+	Vector3 origin;	// 始点
+	Vector3 diff;	// 終点への差分ベクトル
+};
+
+/// <summary>
+/// 線分
+/// </summary>
+struct Segment
+{
+	Vector3 origin;	// 始点
+	Vector3 diff;	// 終点への差分ベクトル
+};
+
+struct Plane
+{
+	Vector3 normal; // 法線
+	float distance; // 距離
+};
+
+struct Triangle
+{
+	Vector3 vertices[3]; // 頂点
+};
+
+Vector3 Perpendicular(const Vector3& _vector);
+
+Plane MakePlane(const Triangle& _triangle);
+
+void DrawLine(const Vector3& _origin, const Vector3& _diff, const Matrix4x4& _viewProjectionMatrix, const Matrix4x4& _viewportMatrix, uint32_t _color);
+void DrawSphere(const Sphere& _sphere, const Matrix4x4& _viewProjectionMatrix, const Matrix4x4& _viewportMatrix, uint32_t _color);
+void DrawPlane(const Plane& _plane, const Matrix4x4& _viewProjectionMatrix, const Matrix4x4& _viewportMatrix, uint32_t _color);
+void DrawTriangle(const Triangle& _triangle, const Matrix4x4& _viewProjectionMatrix, const Matrix4x4& _viewportMatrix, uint32_t _color);
+
+bool IsCollision(const Sphere& _s1, const Sphere& _s2);
+bool IsCollision(const Sphere& _sphere, const Plane& _plane);
+bool IsCollision(const Segment& _segment, const Plane& _plane);
+bool IsCollision(const Line& _line, const Plane& _plane);
+bool IsCollision(const Ray& _ray, const Plane& _plane);
+bool IsCollision(const Segment& _segment, const Triangle& _triangle);
+bool IsCollision(const Segment& _segment, const Triangle& _triangle, const Matrix4x4& _viewProjectionMatrix, const Matrix4x4& _viewportMatrix);
---
+++ 02_04/Vector3/calc/vector3calc.cpp
@@ -0,0 +1,125 @@
+#include "vector3calc.h"
+
+#include <assert.h>
+#include <math.h>
+
+Vector3 Add(const Vector3& _v1, const Vector3& _v2)
+{
+	return Vector3(
+		_v1.x + _v2.x,
+		_v1.y + _v2.y,
+		_v1.z + _v2.z
+	);
+}
+
+Vector3 Subtract(const Vector3& _v1, const Vector3& _v2)
+{
+	return Vector3(
+		_v1.x - _v2.x,
+		_v1.y - _v2.y,
+		_v1.z - _v2.z
+	);
+}
+
+Vector3 Multiply(float _scalar, const Vector3& _v)
+{
+	return Vector3(
+		_scalar * _v.x,
+		_scalar * _v.y,
+		_scalar * _v.z
+	);
+}
+
+float Dot(const Vector3& _v1, const Vector3& _v2)
+{
+	return float(
+		_v1.x * _v2.x +
+		_v1.y * _v2.y +
+		_v1.z * _v2.z
+		);
+}
+
+float Length(const Vector3& _v)
+{
+	return sqrtf(
+		_v.x * _v.x +
+		_v.y * _v.y +
+		_v.z * _v.z
+		);
+}
+
+Vector3 Normalize(const Vector3& _v)
+{
+	float length = Length(_v);
+	return Vector3(
+		_v.x / length,
+		_v.y / length,
+		_v.z / length
+		);
+}
+
+Vector3 Transform(const Vector3& _vector, const Matrix4x4& _matrix)
+{
+	Vector3 result{};
+	result.x = 
+		_vector.x * _matrix.m[0][0] +
+		_vector.y * _matrix.m[1][0] + 
+		_vector.z * _matrix.m[2][0] +
+		1.0f * _matrix.m[3][0];
+	result.y =
+		_vector.x * _matrix.m[0][1] +
+		_vector.y * _matrix.m[1][1] +
+		_vector.z * _matrix.m[2][1] +
+		1.0f * _matrix.m[3][1];
+	result.z =
+		_vector.x * _matrix.m[0][2] +
+		_vector.y * _matrix.m[1][2] +
+		_vector.z * _matrix.m[2][2] +
+		1.0f * _matrix.m[3][2];
+	float w =
+		_vector.x * _matrix.m[0][3] +
+		_vector.y * _matrix.m[1][3] +
+		_vector.z * _matrix.m[2][3] +
+		1.0f * _matrix.m[3][3];
+	assert(w != 0.0f);
+	result.x /= w;
+	result.y /= w;
+	result.z /= w;
+	return result;
+}
+
+Vector3 Cross(const Vector3& _v1, const Vector3& _v2)
+{
+	Vector3 result{};
+	result.x = _v1.y * _v2.z - _v1.z * _v2.y;
+	result.y = _v1.z * _v2.x - _v1.x * _v2.z;
+	result.z = _v1.x * _v2.y - _v1.y * _v2.x;
+
+	return result;
+}
+
+Vector3 Project(const Vector3& _v1, const Vector3& _v2)
+{
+	float v2_norm = Length(_v2);
+	Vector3 v2_unit =
+	{
+		_v2.x / v2_norm,
+		_v2.y / v2_norm,
+		_v2.z / v2_norm
+	};
+
+	Vector3 result{};
+
+	result = Multiply(Dot(_v1, v2_unit), v2_unit);
+
+	return result;
+}
+
+Vector3 ClosestPoint(const Vector3& _point, const Segment& _segment)
+{
+	Vector3 result{};
+
+	result = Add(_segment.origin, Project(Subtract(_point, _segment.origin), _segment.diff));
+
+	return result;
+}
