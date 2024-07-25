72c43323ddaa4f6d657b55663f784f6b66225dc7
Souto-Naitou <146906423+Souto-Naitou@users.noreply.github.com>
2024-07-25 23:26:59 +0900

    ボールがバウンス

---
+++ 04_04/Vector3/calc/vector3calc.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+#include <Matrix4x4.h>
+#include "3d/Object/Object3d.h"
+#include "Vector3/Vector3.h"
+
+// 加算
+Vector3 Add(const Vector3& _v1, const Vector3& _v2);
+
+// 減算
+Vector3 Subtract(const Vector3& _v1, const Vector3& _v2);
+
+// スカラー倍
+Vector3 Multiply(float _scalar, const Vector3& _v);
+
+// 内積
+float Dot(const Vector3& _v1, const Vector3& _v2);
+
+// 長さ（ノルム）
+float Length(const Vector3& _v);
+
+// 正規化
+Vector3 Normalize(const Vector3& _v);
+
+Vector3 Transform(const Vector3& _vector, const Matrix4x4& _matrix);
+
+// クロス積
+Vector3 Cross(const Vector3& _v1, const Vector3& _v2);
+
+// ベクトル射影
+Vector3 Project(const Vector3& _v1, const Vector3& _v2);
+
+// 最接近点
+Vector3 ClosestPoint(const Vector3& _point, const Segment& _segment);
+
+// 線形補間 Linear Interporation
+Vector3 Lerp(const Vector3& _v1, const Vector3& _v2, float _t);
+
+// 反射ベクトル
+Vector3 Reflect(const Vector3& _input, const Vector3& _normal);
